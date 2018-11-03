339689260841e193ef2956f1fade963977d6fa2e
KONA VENKATA SAI PAVAN TEJA <pa1-teja@users.noreply.github.com>
2018-11-03 19:43:15 +0530

    initial commit

---
+++ app/src/main/java/com/pearlcoaching/pearlcoaching/MainActivity.java
@@ -0,0 +1,53 @@
+package com.pearlcoaching.pearlcoaching;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editTextMobile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextMobile = findViewById(R.id.editTextMobile);
+
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextMobile.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextMobile.setError("Enter a valid mobile");
+                    editTextMobile.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(MainActivity.this, VerifyPhoneACtivity.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
